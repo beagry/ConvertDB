964438194c51d34e6c163fb802b9797998edb2ea
Denis Kozlov <deniskozlov@live.ru>
2015-06-02 15:48:43 +0300

    added sqlite instead ms sql

--- TestWSSourceClass/Template workbooks/EFModels/TemplateWbsContext.cs
+++ TestWSSourceClass/Template workbooks/EFModels/TemplateWbsContext.cs
@@ -8,11 +8,12 @@ namespace Converter.Template_workbooks.EFModels
     {
         public TemplateWbsContext() : base("TWBsContext")
         {
-            AppDomain.CurrentDomain.SetData("DataDirectory", Directory.GetCurrentDirectory());
-            Database.SetInitializer(new TemplateWbsInitializer());
+//            AppDomain.CurrentDomain.SetData("DataDirectory", Directory.GetCurrentDirectory());
+//            Database.SetInitializer(new TemplateWbsInitializer());
         }
 
         public DbSet<TemplateWorkbook> TemplateWorkbooks { get; set; }
+
         public DbSet<TemplateColumn> TemplateColumns { get; set; }
 
         public DbSet<BindedColumn> BindedColumns { get; set; } 
